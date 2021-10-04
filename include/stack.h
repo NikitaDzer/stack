c38e13dffac47b3926e9677512ae9c68c7281278
NikitaDzer <nikita.dzer@mail.ru>
2021-10-04 22:36:50 +0300

    Open project

---
+++ include/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by Dzer on 04.10.2021.
+//
+
+#ifndef STACK_H
+#define STACK_H
+
+#endif // STACK_H
---
+++ src/stack.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Dzer on 04.10.2021.
+//
+
+#include "../include/stack.h"
+
+/*!
+ * @brief Typedef for type of stack's data
+ */
+typedef int StackData;
+
